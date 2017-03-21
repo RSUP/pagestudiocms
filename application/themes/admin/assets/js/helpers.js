95beb89483a1a04c23bf222d7dce856ab3f61c93
Cosmo <cosmo@cosmointeractive.co>
2017-03-21 12:46:03 -0400

    Updated base assets

--- application/themes/admin/assets/js/helpers.js
+++ application/themes/admin/assets/js/helpers.js
@@ -1,40 +1,41 @@
-function show_status(text, auto_hide, show_animation)
-{
-    // Init
-    auto_hide = auto_hide !== 'undefined' ? auto_hide : false;
-    show_animation = show_animation !== 'undefined' ? show_animation : false;
+function show_status(text, auto_hide, show_animation){
+  // Init
+  auto_hide = (auto_hide !== 'undefined') ? auto_hide : false;
+  show_animation = show_animation !== 'undefined' ? show_animation : false;
 
-    var status = $('#ajax_status');
+  var status = $('#ajax_status');
 
-    // Show animated gif
-    if (show_animation)
-    {
-        $('#ajax_status_animation').show();
-    }
-    else
-    {
-        $('#ajax_status_animation').hide();
-    }
+  // Show animated gif
+  if (show_animation){
+    $('#ajax_status_animation').show();
+  } else {
+    $('#ajax_status_animation').hide();
+  }
 
-    // Update status text
-    $('#ajax_status_text').html(text);
+  // Update status text
+  $('#ajax_status_text').html(text);
 
-    // Show status box
-    if ( ! status.is(':visible'))
-    {
-        status.fadeIn('slow');
-    }
+  // Show status box
+  if ( ! status.is(':visible')){
+    status.fadeIn('slow');
+  }
 
-    // Hide status box
-    if (auto_hide)
-    {
-        status.delay(1000).fadeOut('slow');
-    }
+  // Hide status box
+  if (auto_hide){
+    status.delay(1000).fadeOut('slow');
+  }
 }
 
-function hide_status()
-{
-    var status = $('#ajax_status');
+function hide_status(){
+  var status = $('#ajax_status');
+  status.fadeOut('slow');
+}
 
-    status.fadeOut('slow');
+/**
+ * On the live edit page, hides the browser scrollbar 
+ */
+function resize_preview_iframe(){
+  var height = $(window).height() - 25;
+  $('#live-editor-iframe').height(height);
+  $(".edit-pane").css("overflow", "hidden");
 }
